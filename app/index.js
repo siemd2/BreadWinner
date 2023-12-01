bfb3ecdf47032f48f675da82f3f09483272a5e6f
Siem Debesay <siem3500@gmail.com>
2023-11-30 23:11:50 -0700

    project setup, added file structure and base styles

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native-web";
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    )
+}
+
+export default Home;
