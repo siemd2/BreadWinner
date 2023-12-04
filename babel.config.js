7fdff03a1bc0b18d0cfbae00dfc3f52ef1f0a51f
Siem Debesay <siem3500@gmail.com>
2023-12-03 18:23:13 -0700

    created custom hook to fetch data from JSearch api

--- babel.config.js
+++ babel.config.js
@@ -6,6 +6,15 @@ module.exports = function (api) {
       "@babel/plugin-proposal-export-namespace-from",
       "react-native-reanimated/plugin",
       require.resolve("expo-router/babel"),
+      // Add the react-native-dotenv plugin configuration below
+      ["module:react-native-dotenv", {
+        moduleName: '@env',
+        path: '.env',
+        blacklist: null,
+        whitelist: null,
+        safe: false,
+        allowUndefined: true,
+      }],
     ],
   };
 };
