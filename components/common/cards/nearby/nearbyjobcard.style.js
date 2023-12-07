756cc0fe96932532636a8f36193dda664c413fb9
Siem Debesay <siem3500@gmail.com>
2023-12-06 23:54:33 -0700

    styling changes to Nearby jobs component stylesheet

--- components/common/cards/nearby/nearbyjobcard.style.js
+++ components/common/cards/nearby/nearbyjobcard.style.js
@@ -10,17 +10,21 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     padding: SIZES.medium,
     borderRadius: SIZES.small,
-    backgroundColor: "#FFF",
+    backgroundColor: COLORS.lightGray, // Slightly off-white to provide contrast
     ...SHADOWS.medium,
-    shadowColor: COLORS.white,
+    shadowColor: COLORS.darkGray, // Darker shadow for better contrast
+    marginBottom: SIZES.base, // Add some margin to separate the cards
+    borderLeftColor: COLORS.primary, // Add a colored accent border on the left
+    borderLeftWidth: 5, // Set the width of the accent border
   },
   logoContainer: {
     width: 50,
     height: 50,
-    backgroundColor: COLORS.white,
+    backgroundColor: COLORS.secondary, // A secondary color for the logo background
     borderRadius: SIZES.medium,
     justifyContent: "center",
     alignItems: "center",
+    shadowColor: COLORS.darkGray, // Darker shadow for the logo container
   },
   logImage: {
     width: "70%",
@@ -33,12 +37,12 @@ const styles = StyleSheet.create({
   jobName: {
     fontSize: SIZES.medium,
     fontFamily: "DMBold",
-    color: COLORS.primary,
+    color: COLORS.darkBlue, // A darker blue for the job name
   },
   jobType: {
     fontSize: SIZES.small + 2,
     fontFamily: "DMRegular",
-    color: COLORS.gray,
+    color: COLORS.darkGray, // Using a dark gray for job type
     marginTop: 3,
     textTransform: "capitalize",
   },
